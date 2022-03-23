14fbb019bd42c21e54f31d43f46a5a359fb48a70
D.D.D <92970450+herokuhabataku@users.noreply.github.com>
2022-03-23 21:13:33 +0900

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import http from 'http';
 import nodeStatic from 'node-static';
 
 
-const bare =  new Server('https://incog.dev/bare/', '');
+const bare =  new Server('/bare/', '');
 const serve = new nodeStatic.Server('static/');
 
 const server = http.createServer();
