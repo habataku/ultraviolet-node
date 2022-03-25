491f28afac76f7c0da4b41066d84cd72ef3230b7
Villager A <100194237+habataku@users.noreply.github.com>
2022-03-25 15:53:59 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://jsoekwoaowowoowoskkwmr43kskee.herokuapp.com/',
+    bare: 'https://jsoekwoaowowoowoskkwmr43kskee.herokuapp.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
